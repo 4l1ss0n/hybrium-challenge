9efdc7292030e3fa58988b72ea22f72b95b59258
Alisson-Py <alissilva1231@hotmail.com>
2021-03-20 15:04:28 -0300

    chore: updated transfer size limit

--- back/src/app/app.ts
+++ back/src/app/app.ts
@@ -3,17 +3,17 @@ import routes from './routes';
 import cors from 'cors';
 import {v2 as cloud} from 'cloudinary';
 import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
 
 dotenv.config();
 cloud.config({
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET,
   cloud_name: process.env.CLOUD_NAME
-})
-
+});
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '10mb'}));
 app.use(routes);
 
 export default app;
